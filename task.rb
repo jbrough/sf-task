b4a3bc479854c4e69fbedffdc91a7cfea2957e3c
Jamie Brough <jamie1612@gmail.com>
2016-08-25 03:51:25 +0100

    add error queue

--- task.rb
+++ task.rb
@@ -3,32 +3,41 @@ require 'uri'
 require 'json'
 require 'digest'
 
-# TODO: Error handling
 class Task
   def self.next
     urls = get_urls
     if urls.empty?
-      return nil
-    else
-      buf = get_image(urls)
-      r = post(storage_uri(urls), buf)
-      return r['Location']
+      return nil, nil
     end
+
+    buf, err = get_image(urls)
+    return handle_err(urls) if err
+
+    r, err = post(storage_url(urls), buf)
+    return handle_err(urls) if err
+
+    return r['Location'], nil
   end
 
-  #private
+  private
 
-  def self.storage_uri(urls)
+  def self.handle_err(urls)
+    post("http://#{ENV['SF_QUEUE_HOST']}/add/error", JSON.parse(urls))
+    # TODO: log errors in post, as failures will be missing from both queues
+    return nil, nil, err
+  end
+
+  def self.storage_url(urls)
     name = Digest::SHA256.hexdigest(urls.to_json)
-    URI.parse("http://#{ENV['SF_STORAGE_HOST']}/#{name}")
+    "http://#{ENV['SF_STORAGE_HOST']}/#{name}"
   end
 
   def self.get_image(urls)
-    get URI.parse("http://#{ENV['SF_MONTAGE_HOST']}/montage?l=#{urls[0]}&r=#{urls[1]}")
+    buf, err = get("http://#{ENV['SF_MONTAGE_HOST']}/montage?l=#{urls[0]}&r=#{urls[1]}")
   end
 
   def self.get_urls
-    r = post(URI.parse("http://#{ENV['SF_QUEUE_HOST']}/next"))
+    r = post("http://#{ENV['SF_QUEUE_HOST']}/next")
     JSON.parse(r.body)
   end
 
@@ -37,7 +46,24 @@ class Task
     res.body
   end
 
-  def self.post(uri, body=nil)
+  def self.get(url)
+    uri = URI(url)
+    res = Net::HTTP.get_response(uri)
+
+    if response_ok?(res)
+      return res.body, nil
+    else
+      return nil, "Bad Download Response"
+    end
+  end
+
+  def self.response_ok?(response)
+    # this implicty checks it's not a 404 etc
+    response.response.content_type == "image/jpeg"
+  end
+
+  def self.post(url, body=nil)
+    uri = URI(url)
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Post.new(uri.path)
     request.body = body
